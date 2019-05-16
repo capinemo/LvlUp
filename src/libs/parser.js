20074dc1656d2a6cf110768e3a17f540b4f538e3
capinemo <capitan__nemo@mail.ru>
2019-05-17 00:22:56 +0300

    Added functions saveFileTags & generateTestsFromTags (proto) + refactored console.log + finished parseFileForTags
    1. Logging in object with functions
    2. Finished parseFileForTags function

--- src/libs/parser.js
+++ src/libs/parser.js
@@ -1,22 +1,46 @@
 const fs = require('fs');
 const path = require('path');
+const ER_M = '\x1b[31m',
+    SC_M = '\x1b[32m',
+    WR_M = '\x1b[33m',
+    ST_M = '\x1b[0m';
+const directives = ['task', 'tag', 'question', 'answer', 'end', 'area', 'level', 'lang'];
 
 exports.test_path = './list/';
 exports.tag_list = {};
-exports.buff_length = 111;
+exports.buff_length = 2048;
 exports.echo_mode = 'work'; // work | test
+exports.list = {}; // work | test
+
+const logIt = {
+    blank: function (type, head, mess) {
+        if (exports.echo_mode === 'work') {
+            console.log(type + head + ST_M + ': ' + mess);
+        }
+    },
+    error: function (head, mess) {
+        this.blank(ER_M, head, mess)
+    },
+    success: function (head, mess) {
+        this.blank(SC_M, head, mess)
+    },
+    warn: function (head, mess) {
+        this.blank(WR_M, head, mess)
+    }
+};
+
+const clearStr = function (str) {
+    return str.length < 12 ? str : (' ' + str).slice(1);
+};
 
 /**
  * Recursively finds *.test files in directory and applies callback function to every file
  *
- * @param callback Function     Function calls for every found item
- * @param path string           Change default path of tests storage
- * @returns void                Nothing
+ * @param   callback Function     Function calls for every found item
+ * @param   path string           Change default path of tests storage
+ * @returns void                  Nothing
  */
 function findTestFilesInDir (callback, path = exports.test_path) {
-    // TODO check rights and log it
-    // Add last / to given path
-
     if (typeof callback !== 'function') {
         throw new Error('parser.findTestFilesInDir: not function given in callback');
     }
@@ -27,7 +51,7 @@ function findTestFilesInDir (callback, path = exports.test_path) {
 
     fs.readdir(path, {withFileTypes : true}, (err, files) => {
         if (err) {
-            return console.log('\x1b[31m' + err + '\x1b[0m');
+            return console.log(ER_M + err + ST_M);
         }
 
         if (!files.length) {
@@ -35,76 +59,114 @@ function findTestFilesInDir (callback, path = exports.test_path) {
         }
 
         files.forEach(function (i) {
+            let full_path = path + i.name;
+
             if (i.isDirectory()) {
-                return findTestFilesInDir(callback,path + i.name + '/');
-            }
+                fs.access(full_path, fs.constants.R_OK | fs.constants.X_OK, (err) => {
+                    return (err)
+                        ? logIt.error('DENIED', full_path)
+                        : findTestFilesInDir(callback,full_path + '/');
+                });
+            } else if (i.isFile() && /\.test$/.test(i.name)) {
+                fs.access(full_path, fs.constants.R_OK, (err) => {
+                    if (err) {
+                        logIt.error('DENIED', full_path);
+                        return;
+                    }
 
-            if (i.isFile() && /\.test$/.test(i.name)) {
-                callback(path + '/' + i.name);
-
-                if (exports.echo_mode === 'work') {
-                    console.log('\x1b[32m' + 'LOADED' + '\x1b[0m: ' + path  + i.name);
-                }
-                return;
+                    callback(full_path);
+                    logIt.success('LOADED', full_path);
+                });
+            } else {
+                logIt.warn('IGNORED', full_path);
             }
-
-            if (exports.echo_mode === 'work') {
-                console.log('\x1b[33m' + 'IGNORED' + '\x1b[0m: ' + path + i.name);
-            }
-            return;
         });
     })
 }
 
+/**
+ * Reads chunks from file, finds @-directives in chunk and saves its to accumulation object.
+ * After file ending calls tests generation function
+ *
+ * @param $file
+ */
 function parseFileForTags ($file) {
-    let stream = fs.createReadStream($file, {encoding: 'utf8', highWaterMark: exports.buff_length}), //start, end
-        chunks = [],
-        count = 0,
-        buffer;
+    let chunk_num = 0,
+        offset = 0,
+        stream = fs.createReadStream($file, {encoding: 'utf8', highWaterMark: exports.buff_length}),
+        buffer = '',
+        tags = {};
 
-    stream.once('error', (err) => {
-        throw err;
-    });
-
-    stream.once('end', () => {
-        //console.log('!!!', chunks.length, chunks instanceof Array)
-        //buffer = Buffer.concat(chunks);
-        //console.log(buffer.length);
-        // Of course, you can do anything else you need to here, like emit an event!
+    stream.on('end', () => {
+        stream.destroy();
+        generateTestsFromTags(tags);
     });
 
     stream.on('data', (chunk) => {
-        let buffer = Buffer.from(chunk);
+        let total_offset = chunk_num * exports.buff_length,
+            chunk_with_buffer = (buffer)
+                ? buffer + chunk
+                : chunk,
+            result = findDirectivesInChunk(chunk_with_buffer, offset);
 
-        if (exports.echo_mode === 'work') {
-            console.log('START: ', count);
+        logIt.success('CHUNK', '#' + chunk_num);
+
+        chunk_num++;
+
+        if (result._hunch && result._hunch.length > 0) {
+            offset = result._hunch[result._hunch.length - 1];
+            buffer = clearStr(chunk.substr(offset - total_offset));
+        } else {
+            offset = total_offset;
+            buffer = '';
         }
 
-        //console.log(findDirectivesInChunk(chunk, count * exports.buff_length));
-        //console.log(buffer);
+        saveFileTags(tags, result);
 
-        //let view   = new Uint8Array(buffer);
-
-        //console.log(buffer.toString('utf8'));
-        //console.log(view.buffer.toString('utf8'));
-        //chunks.push(chunk); // push data chunk to array
-        //console.log(chunk.length, chunks.length);
-        //console.log(chunk);
-
-        //console.log('END: ', count);
-        count++;
+        chunk_with_buffer = null;
+        chunk = null;
     });
+}
+
+/**
+ * Accumulates @-directives from second object in first object
+ *
+ * @param   file_tags Object        List of all file directives
+ * @param   chunk_tags Object       List of directives in the chunk
+ */
+function saveFileTags(file_tags, chunk_tags) {
+    if (!file_tags || Object.prototype.toString.call({file_tags}) !== '[object Object]') {
+        throw new Error('parser.saveFileTags: not object given in first parameter');
+    }
+
+    if (!chunk_tags || Object.prototype.toString.call({chunk_tags}) !== '[object Object]') {
+        throw new Error('parser.saveFileTags: not object given in second parameter');
+    }
+
+    for (let key in chunk_tags) {
+        if (chunk_tags.hasOwnProperty(key) && key !== '_hunch') {
+            if (!file_tags[key] || !Array.isArray(file_tags[key])) {
+                file_tags[key] = [];
+            }
+
+            file_tags[key] = file_tags[key].concat(chunk_tags[key]);
+        }
+    }
+}
+
+function generateTestsFromTags (file_tags) {
+
 }
 
 /**
  * Searching @-directives in the given string. Returns list of the directives with its position in the string
  *
- * @param str string            String for searching directives
- * @param offset integer        Chunk position offset
+ * @param   str string            String for searching directives
+ * @param   offset integer        Chunk position offset
  */
 function findDirectivesInChunk (str = '', offset = 0) {
-    const directives = ['task', 'tag', 'question', 'answer', 'end'],
-        markers = {};
+    //console.log(str);
+    const markers = {};
 
     let substr_list = {substr_10: null, substr_8: null, substr_6: null, substr_5: null, substr_4: null, substr_3: null},
         margin = 0,
@@ -115,6 +177,7 @@ function findDirectivesInChunk (str = '', offset = 0) {
     }
 
     while( ~(mark = str.indexOf('@', margin)) ) {
+        //console.log(mark);
         let directive_found= false;
 
         substr_list.substr_10 = str.substr(mark+1, 10).toLowerCase();
@@ -131,7 +194,7 @@ function findDirectivesInChunk (str = '', offset = 0) {
                 continue;
             }
 
-            if (!markers[item]) { // TODO cover
+            if (!markers[item]) {
                 markers[item] = [];
             }
 
@@ -141,7 +204,7 @@ function findDirectivesInChunk (str = '', offset = 0) {
         }
 
         if (!directive_found) {
-            if (!markers._hunch) { // TODO cover
+            if (!markers._hunch) {
                 markers._hunch = [];
             }
 
@@ -165,5 +228,7 @@ exports.loadTasksTags = function () {
 exports.test = {
     findTestFilesInDir: findTestFilesInDir,
     parseFileForTags: parseFileForTags,
-    findDirectivesInChunk: findDirectivesInChunk
+    findDirectivesInChunk: findDirectivesInChunk,
+    saveFileTags: saveFileTags,
+    generateTestsFromTags: generateTestsFromTags
 };
